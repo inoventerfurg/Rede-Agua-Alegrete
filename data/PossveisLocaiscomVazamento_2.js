a6fddc074f81ab283fd8611ce6098cfe16238d88
paulovictorlisboa <inoventer.furg@gmail.com>
2023-01-25 14:52:26 -0300

    Initial commit

---
+++ data/PossveisLocaiscomVazamento_2.js
@@ -0,0 +1,20 @@
+var json_PossveisLocaiscomVazamento_2 = {
+"type": "FeatureCollection",
+"name": "PossveisLocaiscomVazamento_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "VALUE": 1.0, "Rua": "Av. Assis Brasil, 27" }, "geometry": { "type": "Point", "coordinates": [ -55.798557912652434, -29.788998678582878 ] } },
+{ "type": "Feature", "properties": { "VALUE": 1.0, "Rua": "Av. Assis Brasil, 1003" }, "geometry": { "type": "Point", "coordinates": [ -55.801402779991371, -29.797687434181679 ] } },
+{ "type": "Feature", "properties": { "VALUE": 1.0, "Rua": "Av. Tiaraju, 511" }, "geometry": { "type": "Point", "coordinates": [ -55.769874426334368, -29.78697523417804 ] } },
+{ "type": "Feature", "properties": { "VALUE": 1.0, "Rua": "Rua Gen. Sampaio, 708" }, "geometry": { "type": "Point", "coordinates": [ -55.793352181505838, -29.783130860593744 ] } },
+{ "type": "Feature", "properties": { "VALUE": 1.0, "Rua": "Rua dos Andradas, 599" }, "geometry": { "type": "Point", "coordinates": [ -55.794908584257321, -29.782738981038253 ] } },
+{ "type": "Feature", "properties": { "VALUE": 1.0, "Rua": "Rua Gen. Sampaio, 1148" }, "geometry": { "type": "Point", "coordinates": [ -55.793804661491386, -29.78421785580025 ] } },
+{ "type": "Feature", "properties": { "VALUE": 1.0, "Rua": "Rua Dr. Quintana, 34" }, "geometry": { "type": "Point", "coordinates": [ -55.791781138125579, -29.784740811916937 ] } },
+{ "type": "Feature", "properties": { "VALUE": 1.0, "Rua": "Rua Luiz de Freitas, 422" }, "geometry": { "type": "Point", "coordinates": [ -55.789205379404443, -29.779573635539165 ] } },
+{ "type": "Feature", "properties": { "VALUE": 1.0, "Rua": "Rua Dr. Lauro Dornelles, 276" }, "geometry": { "type": "Point", "coordinates": [ -55.797139518731676, -29.784850587322133 ] } },
+{ "type": "Feature", "properties": { "VALUE": 1.0, "Rua": "Rua Joaquim Nabuco, 107" }, "geometry": { "type": "Point", "coordinates": [ -55.788109561933027, -29.780375745641084 ] } },
+{ "type": "Feature", "properties": { "VALUE": 1.0, "Rua": "Rua Dr. Lauro Dornelles, 521" }, "geometry": { "type": "Point", "coordinates": [ -55.796309822670636, -29.782345670367263 ] } },
+{ "type": "Feature", "properties": { "VALUE": 1.0, "Rua": "Rua Aloísio dos Santos Morais, 226" }, "geometry": { "type": "Point", "coordinates": [ -55.770433843681985, -29.791988669023052 ] } },
+{ "type": "Feature", "properties": { "VALUE": 1.0, "Rua": "Rua Gen. Sampaio, 762" }, "geometry": { "type": "Point", "coordinates": [ -55.792757550689238, -29.780959699451071 ] } }
+]
+}
